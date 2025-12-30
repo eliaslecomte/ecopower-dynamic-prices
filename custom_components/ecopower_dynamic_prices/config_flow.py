010cb34dd77e7d4523abb6ffbb7ef18081a043ee
eliaslecomte <elias.lecomte@gmail.com>
2025-12-30 12:28:57 +0100

    fix: config loading bug

--- custom_components/ecopower_dynamic_prices/config_flow.py
+++ custom_components/ecopower_dynamic_prices/config_flow.py
@@ -315,15 +315,11 @@ class EcopowerDynamicPricesConfigFlow(
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Get the options flow for this handler."""
-        return EcopowerDynamicPricesOptionsFlow(config_entry)
+        return EcopowerDynamicPricesOptionsFlow()
 
 
 class EcopowerDynamicPricesOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for Ecopower Dynamic Prices."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
